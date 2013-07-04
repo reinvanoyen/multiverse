83fb00e1dcdb8d063c73fdeb868af37ff2a987e3
Rein Van Oyen <rein@bakboord.be>
2013-07-04 21:03:57 +0200

    player faster

--- src/active_objects/Player.js
+++ src/active_objects/Player.js
@@ -8,19 +8,19 @@ var Player = Drawable.extend( {
 	{
 		if( Game.input_manager.is_key_down( 37 ) )
 		{
-			this.sprite.position.x = this.sprite.position.x - 4;
+			this.sprite.position.x = this.sprite.position.x - 6;
 		}
 		if( Game.input_manager.is_key_down( 39 ) )
 		{
-			this.sprite.position.x = this.sprite.position.x + 4;
+			this.sprite.position.x = this.sprite.position.x + 6;
 		}
 		if( Game.input_manager.is_key_down( 40 ) )
 		{
-			this.sprite.position.y = this.sprite.position.y + 4;
+			this.sprite.position.y = this.sprite.position.y + 6;
 		}
 		if( Game.input_manager.is_key_down( 38 ) )
 		{
-			this.sprite.position.y = this.sprite.position.y - 4;
+			this.sprite.position.y = this.sprite.position.y - 6;
 		}
 	}
 
