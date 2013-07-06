a7aa73823065dae11c7dd1f6d07c6d7bdc7133c3
Rein Van Oyen <rein@bakboord.be>
2013-07-06 05:08:22 +0200

    added sound + healthsystem

--- src/active_objects/Player.js
+++ src/active_objects/Player.js
@@ -3,6 +3,22 @@ var Player = PhysicsObject.extend( {
 	constructor: function()
 	{
 		this.base( 'textures/player.png' );
+		this.health = 100;
+	},
+	setHealth: function( n )
+	{
+		var health_difference = n - this.health;
+		this.health = n;
+		Game.ui.healthbar.setValue( this.health );
+		
+		if( health_difference < 0  )
+		{
+			Game.ui.log.addLine( health_difference + ' health', 'error' );
+		}
+		else
+		{
+			Game.ui.log.addLine( '+' + health_difference + ' health', 'success' );
+		}
 	},
 	update: function()
 	{
@@ -25,23 +41,41 @@ var Player = PhysicsObject.extend( {
 			this.velocity_y = Math.max( this.velocity_y - 0.05, 0 );
 		}
 		
-		if( Game.input_manager.is_key_down( 37 ) )
+		if(
+			Game.input_manager.is_key_down( 37 )
+			|| Game.input_manager.is_key_down( 38 )
+			|| Game.input_manager.is_key_down( 39 )
+			|| Game.input_manager.is_key_down( 40 )
+		)
 		{
-			this.velocity_y = Math.max( this.velocity_y - 0.1, -15 );
-			this.velocity_x = Math.max( this.velocity_x - 0.2, -15 );
+			if( Game.input_manager.is_key_down( 37 ) )
+			{
+				this.velocity_y = Math.max( this.velocity_y - 0.1, -15 );
+				this.velocity_x = Math.max( this.velocity_x - 0.2, -15 );
+				Game.universe.sounds.get( 'booster' ).play();
+			}
+			if( Game.input_manager.is_key_down( 39 ) )
+			{
+				this.velocity_y = Math.max( this.velocity_y - 0.1, -15 );
+				this.velocity_x = Math.min( this.velocity_x + 0.2, 15 );
+				Game.universe.sounds.get( 'booster' ).play();
+			}
+			
+			if( Game.input_manager.is_key_down( 40 ) )
+			{
+				this.velocity_y = Math.min( this.velocity_y + 0.2, 15 );
+				Game.universe.sounds.get( 'booster' ).play();
+			}
+			
+			if( Game.input_manager.is_key_down( 38 ) )
+			{
+				this.velocity_y = Math.max( this.velocity_y - 0.3, -15 );
+				Game.universe.sounds.get( 'booster' ).play();
+			}
 		}
-		if( Game.input_manager.is_key_down( 39 ) )
+		else
 		{
-			this.velocity_y = Math.max( this.velocity_y - 0.1, -15 );
-			this.velocity_x = Math.min( this.velocity_x + 0.2, 15 );
-		}
-		if( Game.input_manager.is_key_down( 40 ) )
-		{
-			this.velocity_y = Math.min( this.velocity_y + 0.2, 15 );
-		}
-		if( Game.input_manager.is_key_down( 38 ) )
-		{
-			this.velocity_y = Math.max( this.velocity_y - 0.2, -15 );
+			Game.universe.sounds.get( 'booster' ).stop();
 		}
 		
 		this.sprite.rotation = this.velocity_x / 10;
