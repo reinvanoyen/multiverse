b0930831095f030e70872fffcb6eecccf12d3b79
Rein Van Oyen <rein@bakboord.be>
2013-07-06 05:08:03 +0200

    added background + tweaked size of universe + added sounds

--- src/Universe.js
+++ src/Universe.js
@@ -14,27 +14,35 @@ var Universe = Base.extend( {
 		var theme_sound = new Sound( 'sound/music/theme.mp3', 'audio/mpeg' );
 		theme_sound.loop();
 		this.sounds.add( 'theme', theme_sound );
+		this.sounds.add( 'sidethruster', new Sound( 'sound/ship/sidethruster1.wav', 'audio/wav' ) );
+		this.sounds.add( 'booster', new Sound( 'sound/ship/booster1.wav', 'audio/wav' ) );
 		this.sounds.get( 'theme' ).play();
 		
 		// Solar systems
 		this.solar_systems = new DrawableStorage();
-		this.addSolarSystems( 20 );
+		this.addSolarSystems( 10 );
 		
 		// Scrolling
 		this.bindScroll();
+		
+		// Set background
+		var bg = PIXI.Sprite.fromImage( 'textures/background.png' );
+		bg.width = Game.width;
+		bg.height = Game.height;
 		
 		// Draw
 		this.camera.follow( this.player );
 		this.player.draw( this.stage );
 		this.solar_systems.drawAll( this.stage );
+		Game.stage.addChild( bg );
 		Game.stage.addChild( this.stage );
 	},
 	addSolarSystems: function( n )
 	{
 		for( var i = 1; i <= n; i++ )
 		{
-			var solar = new SolarSystem( randomInt( -100000, 100000 ), randomInt( -100000, 100000 ) );
-			solar.addPlanets( 10 );
+			var solar = new SolarSystem( randomInt( -20000, 20000 ), randomInt( -20000, 20000 ) );
+			solar.addPlanets( 15 );
 			this.solar_systems.add( 'solar_' + i, solar );
 		}
 	},
