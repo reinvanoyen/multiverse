a5bb3ebf9835bbd61796e1f506aaea331656cf55
Rein Van Oyen <rein@bakboord.be>
2013-07-06 17:20:08 +0200

    waypoint fix to hide arrow when close to waypoint

--- src/ui/Waypoint.js
+++ src/ui/Waypoint.js
@@ -10,13 +10,22 @@ var Waypoint = Drawable.extend( {
 	update: function()
 	{
 		var angle = Game.universe.camera.follow_object.position.getAngle( this.position );
+		var distance = Game.universe.camera.follow_object.position.getDistance( this.position );
 		
-		this.arrow.setPosition( Game.width / 2, Game.height / 2 );
+		if( distance < 500 )
+		{
+			this.arrow.sprite.visible = false;
+		}
+		else
+		{
+			this.arrow.sprite.visible = true;
+			this.arrow.setPosition( Game.width / 2, Game.height / 2 );
 		
-		this.sprite.scale.x = 1 / Game.universe.camera.zoom;
-		this.sprite.scale.y = 1 / Game.universe.camera.zoom;
+			this.sprite.scale.x = 1 / Game.universe.camera.zoom;
+			this.sprite.scale.y = 1 / Game.universe.camera.zoom;
 		
-		this.arrow.sprite.rotation = -angle;
+			this.arrow.sprite.rotation = -angle;
+		}
 	}
 
 } );
