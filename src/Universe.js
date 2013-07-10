ea17fa90b9aa87e48e60b1760cd503ef71adbb9e
Rein Van Oyen <rein@bakboord.be>
2013-07-10 22:29:40 +0200

    layers added

--- src/Universe.js
+++ src/Universe.js
@@ -19,19 +19,29 @@ var Universe = Base.extend( {
 		
 		// Add some allies
 		this.ally = new Ally();
+		this.ally.setTarget( this.player );
 		this.ally.setPosition( 400, -400 );
-		this.ally.follow( this.player );
+		this.ally.setState( 'following' );
 		
+		// Add some allies
 		this.ally2 = new Ally();
-		this.ally2.setPosition( 300, 300 );
-		this.ally2.follow( this.ally );
+		this.ally2.setTarget( this.ally );
+		this.ally2.setPosition( 600, -600 );
+		this.ally2.setState( 'following' );
+		
+		this.debris1 = new Layer( 'textures/layers/clouds.png', 0.9, this.stage );
+		this.debris2 = new Layer( 'textures/layers/clouds.png', 1, this.stage );
+		this.debris3 = new Layer( 'textures/layers/clouds.png', 1.2, this.stage );
 		
 		// Draw
+		this.debris1.draw( Game.stage );
+		this.debris2.draw( Game.stage );
+		this.solar_systems.drawAll( this.stage );
 		this.ally.draw( this.stage );
 		this.ally2.draw( this.stage );
 		this.player.draw( this.stage );
-		this.solar_systems.drawAll( this.stage );
 		Game.stage.addChild( this.stage );
+		this.debris3.draw( Game.stage );
 	},
 	addSolarSystems: function( n )
 	{
@@ -47,6 +57,9 @@ var Universe = Base.extend( {
 	update: function()
 	{
 		this.age++;
+		this.debris1.update();
+		this.debris2.update();
+		this.debris3.update();
 		this.solar_systems.updateAll();
 		this.ally.update();
 		this.ally2.update();
